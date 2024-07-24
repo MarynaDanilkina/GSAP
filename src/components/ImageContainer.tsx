9963f9f0f395b07ba8d41002819234e319d4cbb9
Stanislav <kepamuk@yandex.ru>
2024-07-24 14:31:25 +0300

    feat: index

--- src/components/ImageContainer.tsx
+++ src/components/ImageContainer.tsx
@@ -1,65 +1,67 @@
-"use client";
+"use client"
 
-import {memo, MutableRefObject, RefObject, useRef} from "react";
-import Image from "next/image";
-import gsap from "gsap";
-import {useGSAP} from '@gsap/react';
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import Image from "next/image"
+import { memo, MutableRefObject, RefObject, useRef } from "react"
 
-import type {ISlide} from "@/constants/main";
+import type { ISlide } from "@/constants/main"
 
-import styles from "./ImageContainer.module.scss";
+import styles from "./ImageContainer.module.scss"
 
 interface ImageContainerProps {
-    mainSlideRef: RefObject<HTMLImageElement>;
-    slide: ISlide;
-    slideRefs: MutableRefObject<(HTMLDivElement | null)[]>;
-    index: number;
-    imageContainerRef: RefObject<HTMLDivElement>;
+  mainSlideRef: RefObject<HTMLImageElement>
+  slide: ISlide
+  slideRefs: MutableRefObject<(HTMLDivElement | null)[]>
+  index: number
+  imageContainerRef: RefObject<HTMLDivElement>
 }
 
-export const ImageContainer = memo(({
-                                        mainSlideRef,
-                                        slideRefs,
-                                        slide,
-                                        index,
-                                        imageContainerRef
-                                    }: ImageContainerProps) => {
-            const setImageRef = useRef<HTMLDivElement>(null);
+export const ImageContainer = memo(
+  ({
+    mainSlideRef,
+    slideRefs,
+    slide,
+    index,
+    imageContainerRef,
+  }: ImageContainerProps) => {
+        const setImageRef = useRef<HTMLDivElement>(null)
+        
+        console.log(index)
 
-            useGSAP(() => {
-                    const slideRef = index === 0 ? mainSlideRef.current : slideRefs.current[index - 1];
+    useGSAP(() => {
+      const slideRef =
+        index === 0 ? mainSlideRef.current : slideRefs.current[index - 1]
 
-                    if (index === 1) {
-                        console.log(slideRef)
-                    }
+      if (index === 1) {
+        console.log(slideRef)
+      }
 
-                    if (setImageRef.current) {
-                        gsap.to(setImageRef.current, {
-                            scrollTrigger: {
-                                trigger: slideRef,
-                                start: "top top",
-                                end: "bottom center",
-                                scrub: true,
-                                markers: true,
-                            },
-                            opacity: 1,
-                        });
-                    }
-                }, [{scope: imageContainerRef}, index]
-            );
+      if (setImageRef.current) {
+        gsap.to(setImageRef.current, {
+          scrollTrigger: {
+            trigger: slideRef,
+            start: "top top",
+            end: "bottom center",
+            scrub: true,
+            markers: true,
+          },
+          opacity: 1,
+        })
+      }
+    }, [{ scope: imageContainerRef }, index])
 
-            return (
-                <div key={index} className={styles.imageContainer} ref={setImageRef}>
-                    <Image
-                        src={slide.image}
-                        alt={"project image"}
-                        className={styles.image}
-                        priority
-                    />
-                </div>
-            )
-        }
+    return (
+      <div key={index} className={styles.imageContainer} ref={setImageRef}>
+        <Image
+          src={slide.image}
+          alt={"project image"}
+          className={styles.image}
+          priority
+        />
+      </div>
     )
-;
+  }
+)
 
-ImageContainer.displayName = "ImageContainer";
+ImageContainer.displayName = "ImageContainer"
